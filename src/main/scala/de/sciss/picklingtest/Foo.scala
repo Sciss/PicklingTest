869314770b372038e0dc4d7b8df16b3e15ef662b
Hanns Holger Rutz <contact@sciss.de>
2014-02-25 21:54:22 +0100

    yo, NPE. End of travel with Scala Pickling I guess...

--- src/main/scala/de/sciss/picklingtest/Foo.scala
+++ src/main/scala/de/sciss/picklingtest/Foo.scala
@@ -3,4 +3,6 @@ package de.sciss.picklingtest
 trait Foo {
   def name: String
   def age: Int
+
+  override def toString = s"Foo($name, $age)"
 }
--- src/main/scala/de/sciss/picklingtest/Main.scala
+++ src/main/scala/de/sciss/picklingtest/Main.scala
@@ -8,4 +8,9 @@ object Main extends App {
   val p = f.pickle
   val g = p.unpickle[Foo]
   println(g)
+
+  val h: Foo = new impl.FooImpl2("bar" -> 1234)
+  val q = h.pickle
+  val j = q.unpickle[Foo]
+  println(j)
 }
